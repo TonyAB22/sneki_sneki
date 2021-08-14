9212757703578f5eec20028daadd3413bc3e1b18
AYOWA DEVEZA <lowaramirez@gmail.com>
2021-08-14 13:25:03 +0800

    Lowa

--- snakes.py
+++ snakes.py
@@ -8,33 +8,42 @@ import random
 # Initializing Screen
 win = turtle.Screen()
 win.title("Sneki's Game") #screen title
-win.bgcolor(Lavander) #background color
+win.bgcolor("violet") #background color
 win.setup(w=500,h=500) #window width, height
 win.tracer(0) #turn off screen updates
 
 # Keyboard Bindings
 win.keypad(True) #enable keypad
 win.listen()
-win.onkeypress(go_up, "w") #press w for up
-win.onkeypress(go_down, "s") #press s for down
-win.onkeypress(go_right, "d") #press d for right
-win.onkeypress(go_left, "a") #press a for left
-
-
-
+win.onkeypress(go_up, "w") #bindings for up
+win.onkeypress(go_down, "s") #bindings for down
+win.onkeypress(go_right, "d") #bindings for right
+win.onkeypress(go_left, "a") #bindings for left
 
 #Initiate Values
-key = KEY_RIGHT #
-score = 0 #
+key = KEY_RIGHT #wala ko kasabot ngano key right
+score = 0 #default scoring
 
-# Snake Food
+# Snake Head Values
+head = turtle.Turtle()
+head speed (0) #snake speed
+head.shape("square") #snake shape 
+head.color("black") #snake color
+head.penup()
+head.goto(0,100)
+head.direction = "stop"
 
+# Snake Food Values 
+food = turtle.Turtle()
+food.speed (0)
+food.shape("triangle")
+food.color("blue")
+food.penup(0)
+food.goto(0,100)
 
 # Initialize Startup Food and Snake Coordinates
 snake = [[0,0], [0,0], [0,0] #input values for snake
 food = [0,0]
-
-
 
 
 # Misc. Codes Haha
