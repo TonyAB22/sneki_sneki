a6e63264ef5eb52424cfb8a0de5a948dafe2348a
AYOWA DEVEZA <lowaramirez@gmail.com>
2021-08-14 13:06:54 +0800

    Lowa

--- snakes.py
+++ snakes.py
@@ -12,11 +12,25 @@ win.bgcolor(Lavander) #background color
 win.setup(w=500,h=500) #window width, height
 win.tracer(0) #turn off screen updates
 
+# Keyboard Bindings
+win.keypad(True) #enable keypad
+win.listen()
+win.onkeypress(go_up, "w") #press w for up
+win.onkeypress(go_down, "s") #press s for down
+win.onkeypress(go_right, "d") #press d for right
+win.onkeypress(go_left, "a") #press a for left
+
+
+
+
 #Initiate Values
 key = KEY_RIGHT #
 score = 0 #
 
-#Initialize Startup Food and Snake Coordinates
+# Snake Food
+
+
+# Initialize Startup Food and Snake Coordinates
 snake = [[0,0], [0,0], [0,0] #input values for snake
 food = [0,0]
 
