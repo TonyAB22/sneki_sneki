7717c91b796342edd80fca17ec6a4b41b1a276d8
AYOWA DEVEZA <lowaramirez@gmail.com>
2021-08-15 02:04:29 +0800

    Lowa

--- snakes.py
+++ snakes.py
@@ -8,13 +8,13 @@ import random
 # Initializing Screen
 win = turtle.Screen()
 win.title("Sneki's Game") #screen title
-win.bgcolor("violet") #background color
+win.bgcolor("purple") #background color
 win.setup(w=500,h=500) #window width, height
 win.tracer(0) #turn off screen updates
 
 # Keyboard Bindings
 win.keypad(True) #enable keypad
-win.listen()
+win.listen() #
 win.onkeypress(go_up, "w") #bindings for up
 win.onkeypress(go_down, "s") #bindings for down
 win.onkeypress(go_right, "d") #bindings for right
@@ -25,25 +25,28 @@ key = KEY_RIGHT #wala ko kasabot ngano key right
 score = 0 #default scoring
 
 # Snake Head Values
-head = turtle.Turtle()
-head.speed (0) #snake speed
+head = turtle.Turtle() #snake 
+head.speed (0) #snake movement
 head.shape("square") #snake shape 
-head.color("black") #snake color
-head.penup()
+head.color("white") #snake color
+head.penup() #path taken by snake
 head.goto(0,100)
 head.direction = "stop"
 
 # Snake Food Values 
 food = turtle.Turtle()
 food.speed (0)
-food.shape("triangle")
-food.color("blue")
-food.penup(0)
+food.shape("circle")
+food.color("black")
+food.penup(0) #path taken by food
 food.goto(0,100)
 
 # Initialize Startup Food and Snake Coordinates
 snake = [[0,0], [0,0], [0,0] #input values for snake
-food = [0,0]
+food = [0,0] #input values for food
+
+# Display Score and Title
+
 
 
 # Misc. Codes Haha
