3eeadc8b78f180bac8a1ef2bd9ef3c2e328e0e6b
AYOWA DEVEZA <lowaramirez@gmail.com>
2021-08-14 12:41:35 +0800

    Lowa

--- snakes.py
+++ snakes.py
@@ -13,21 +13,19 @@ win.setup(w=500,h=500) #window width, height
 win.tracer(0) #turn off screen updates
 
 #Initialize Values
+key = KEY_RIGHT #
+score = 0 #
+
+#
+
+
+
+# Misc. Codes Haha
 from curses. import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
-
 window.keypad(True) #enable keypad
-curses.noecho() #turn off automatic echoing of keys to the screen
 curses.curs_set(0)
-window.nodelay(True) #do not wait for the user input
+
 win = tintinoo
-
-#for games
-
-
-
-#initiate values
-key = KEY_RIGHT
-score = 0
 whyarewehere = "just to sufffer"
 HAHAHHAAHA
 
