8e274a410c63c7983c714bfca7f06fbf3784afe0
AYOWA DEVEZA <lowaramirez@gmail.com>
2021-08-14 11:57:48 +0800

    Lowa

--- snakes.py
+++ snakes.py
@@ -1,18 +1,17 @@
 import curses
-
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-<<<<<<< HEAD
-from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
-from random import randint
+from curses. import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 curses.initscr() #initialize screen
-window = curses.newwin(30, 60, 0, 0) #create new window H=30, W=60
+win = curses.newwin(20, 60, 0, 0) #create new window H=20, W=60
 window.keypad(True) #enable keypad
 curses.noecho() #turn off automatic echoing of keys to the screen
 curses.curs_set(0)
 window.nodelay(True) #do not wait for the user input
-=======
 win = tintinoo
 
->>>>>>> 1578ae50b5750816ac5cf34198d284d17cb7de41
 #for games
+
+
+
+#initiate values
+key = KEY_RIGHT
+score = 0
